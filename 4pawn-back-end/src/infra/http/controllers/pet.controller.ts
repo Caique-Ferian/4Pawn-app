8540ea0b86083dc0392e49cb6363ceb72017f734
Caique Ferian <caiquegsf@gmail.com>
2023-03-01 15:13:06 -0300

    Adiciona rota POST dos pets com seu respectivo DTO verificado pelo class-validator

--- 4pawn-back-end/src/infra/http/controllers/pet.controller.ts
+++ 4pawn-back-end/src/infra/http/controllers/pet.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import CreatePet from '@app/use-cases/pet/create-pet';
 import FindAllPets from '@app/use-cases/pet/find-all-pets';
 import UpdatePet from '@app/use-cases/pet/update-pet';
 import { PetHTTP } from '../view-module/types';
 import PetViewModule from '../view-module/pets-view-module';
+import CreatePetBody from '../dtos/pet/create-pet-body';
 
 @Controller('pets')
 export class PetController {
@@ -12,6 +13,19 @@ export class PetController {
     private findAllPets: FindAllPets,
     private updatePet: UpdatePet,
   ) {}
+
+  @Post()
+  async create(@Body() body: CreatePetBody): Promise<{ pet: PetHTTP }> {
+    const { name, ageInYears, image, weightInKg, color } = body;
+    const { pet } = await this.createPet.execute({
+      name,
+      ageInYears,
+      image,
+      weightInKg,
+      color,
+    });
+    return { pet: PetViewModule.toHTTP(pet) };
+  }
 
   @Get()
   async findAll(): Promise<{ pets: PetHTTP[] }> {
